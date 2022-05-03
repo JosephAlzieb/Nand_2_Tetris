adee89a6554dcf9ca36afab6bcc76aea833b81c4
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-04 01:43:54 +0200

    in this loop we read the A-, and C-instruction and write his binary code in a hack file

--- Project_06/src/assembler/Main.java
+++ Project_06/src/assembler/Main.java
@@ -1,12 +1,17 @@
 package assembler;
 
+import static assembler.InstructionsType.A_INSTRUCTION;
+import static assembler.InstructionsType.C_INSTRUCTION;
 import static assembler.InstructionsType.LABEL;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    String fileName = "";
+    String fileName = "this come from args ... later";
     Parser parser = new Parser(fileName);
 
     SymbolTable symbolTable = new SymbolTable();
@@ -19,6 +24,47 @@ public class Main {
         symbolTable.addSymbolToAddress(parser.symbol().substring(1, length - 1), line);
         parser.removeInstruction(line);
       }
+    }
+
+    parser.reset();
+
+    // example: Add.asm ==> Add
+    String fileNameSplit = fileName.substring(0, fileName.length() - 4);
+    try (FileWriter fileWriter = new FileWriter(fileNameSplit + ".hack")) {
+      while (parser.hasMoreCommands()) {
+        String currentCommand = parser.getNextCommand();
+        StringBuilder commands = new StringBuilder();
+        String binary;
+        if (parser.commandType().equals(A_INSTRUCTION)
+            && ((Character.isLetter(parser.symbol().charAt(0)))
+            && symbolTable.contains(parser.symbol()))) {
+
+          int address = symbolTable.getAddress(parser.symbol());
+          binary = Integer.toBinaryString(address);
+          commands.append(binary);
+        } else if (parser.commandType().equals(A_INSTRUCTION)
+            && Character.isLetter(parser.symbol().charAt(0))
+            && !symbolTable.contains(parser.symbol())) {
+
+          symbolTable.addSymbol(parser.symbol());
+          int address = symbolTable.getAddress(parser.symbol());
+          binary = Integer.toBinaryString(address);
+          commands.append(binary);
+        } else if (parser.commandType().equals(C_INSTRUCTION)) {
+          String strComp = parser.comp();
+          String strDest = parser.dest();
+          String strJump = parser.jump();
+          commands.append("111");
+//          sbCommand.append(Code.comp(strComp));
+//          sbCommand.append(Code.dest(strDest));
+//          sbCommand.append(Code.jump(strJump));
+        } else {
+          commands.append("error");
+        }
+        fileWriter.write(commands + "\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
