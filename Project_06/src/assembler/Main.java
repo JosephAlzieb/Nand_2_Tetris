3743f5e77ad9a9a6b4d351d523cae0bbee4c3080
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-05 10:15:36 +0200

    fill the  short command wit zeros

--- Project_06/src/assembler/Main.java
+++ Project_06/src/assembler/Main.java
@@ -10,12 +10,12 @@ import java.io.IOException;
 public class Main {
 
   public static void main(String[] args) {
-    String fileName = "C:\\Users\\Joseph\\Documents\\Von NAND zu Tetris 2022\\nand2tetris\\nand2tetris\\projects\\06\\max\\Max.asm";
-//    if (args.length == 0) {
-//      return;
-//    } else {
-//      fileName = args[args.length - 1];
-//    }
+    String fileName;  //"C:\\Users\\Joseph\\Documents\\Von NAND zu Tetris 2022\\nand2tetris\\nand2tetris\\projects\\06\\max\\Max.asm";
+    if (args.length == 0) {
+      return;
+    } else {
+      fileName = args[args.length - 1];
+    }
 
     Parser parser = new Parser(fileName);
 
@@ -42,11 +42,11 @@ public class Main {
     System.out.println("translation from Asm to Bin");
     System.out.println("=============================");
     // example: Add.asm ==> Add
-    String fileNameSplit = fileName.substring(0, fileName.length() - 4);
-    try (FileWriter fileWriter = new FileWriter("max.hack")) {
+    // String fileNameSplit = fileName.substring(0, fileName.length() - 4);
+    try (FileWriter fileWriter = new FileWriter("result.hack")) {
       while (parser.hasMoreCommands()) {
-        String currentCommand = parser.getNextCommand();
-        StringBuilder commands = new StringBuilder();
+        parser.getNextCommand(); // return the current command, and goes one further.
+        StringBuilder command = new StringBuilder();
         String binary;
         String symbol = parser.symbol();
         if (parser.commandType().equals(A_INSTRUCTION)
@@ -54,7 +54,7 @@ public class Main {
             && symbolTable.contains(symbol))) {
           int address = symbolTable.getAddress(symbol);
           binary = Integer.toBinaryString(address);
-          commands.append(binary);
+          command.append(binary);
         } else if (parser.commandType().equals(A_INSTRUCTION)
             && Character.isLetter(symbol.charAt(0))
             && !symbolTable.contains(symbol)) {
@@ -62,27 +62,30 @@ public class Main {
           symbolTable.addSymbol(symbol);
           int address = symbolTable.getAddress(symbol);
           binary = Integer.toBinaryString(address);
-          commands.append(binary);
+          command.append(binary);
         } else if (parser.commandType().equals(A_INSTRUCTION)) {
           binary = Integer.toBinaryString(Integer.parseInt(symbol));
-          commands.append(binary);
+          command.append(binary);
         } else if (parser.commandType().equals(LABEL)){
           int address = symbolTable.contains(symbol) ? symbolTable.getAddress(symbol):4444;
           binary = Integer.toBinaryString(address);
-          commands.append(binary);
+          command.append(binary);
         } else if (parser.commandType().equals(C_INSTRUCTION)) {
           String strComp = parser.comp();
           String strDest = parser.dest();
           String strJump = parser.jump();
-          commands.append("111");
-          commands.append(Code.comp(strComp));
-          commands.append(Code.dest(strDest));
-          commands.append(Code.jump(strJump));
+          command.append("111");
+          command.append(Code.comp(strComp));
+          command.append(Code.dest(strDest));
+          command.append(Code.jump(strJump));
         } else {
-          commands.append("Error");
+          command.append("Error");
         }
-        System.out.println(commands);
-        fileWriter.write(commands + "\n");
+        if (command.length() < 15){
+          command = new StringBuilder("0".repeat(Math.max(0, 16 - command.length())) + command);
+        }
+        System.out.println(command);
+        fileWriter.write(command + "\n");
       }
     } catch (IOException e) {
       e.printStackTrace();
