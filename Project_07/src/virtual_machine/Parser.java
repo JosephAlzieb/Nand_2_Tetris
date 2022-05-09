219dd5e31085b84646c1c4b76a1cccfc3de28758
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-09 17:31:46 +0200

    to read files with scanner

--- Project_07/src/virtual_machine/Parser.java
+++ Project_07/src/virtual_machine/Parser.java
@@ -1,5 +1,21 @@
 package virtual_machine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Parser {
+
+  private Scanner scanner;
+
+  public Parser(File file) {
+    try {
+      scanner = new Scanner(new FileReader(file));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
 
 }
