dac08b21b5f674ba707b0e4719a5d0b78395e751
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-10 11:21:15 +0200

    in this loop we will write the assembly code

--- Project_07/src/virtual_machine/Main.java
+++ Project_07/src/virtual_machine/Main.java
@@ -1,5 +1,9 @@
 package virtual_machine;
 
+import static virtual_machine.CommandType.C_ARITHMETIC;
+import static virtual_machine.CommandType.C_POP;
+import static virtual_machine.CommandType.C_PUSH;
+
 import java.io.File;
 import java.util.ArrayList;
 
@@ -9,18 +13,17 @@ public class Main {
     File fileIn = new File(args[0]);
     File fileOut;
     ArrayList<File> files = new ArrayList<>();
-    if (args.length != 1)
+    if (args.length != 1) {
       throw new IllegalArgumentException("there is no files to translate");
-    else if (fileIn.isFile() && !(args[0].endsWith(".vm")))
+    } else if (fileIn.isFile() && !(args[0].endsWith(".vm"))) {
       throw new IllegalArgumentException("incorrect file type.");
-    else if (fileIn.isFile() && args[0].endsWith(".vm")) {
+    } else if (fileIn.isFile() && args[0].endsWith(".vm")) {
       files.add(fileIn);
-    } else
-    {
+    } else {
       // get Files with .vm in the directory
       files = getVMFiles(fileIn);
     }
-    fileOut = new File( "result.asm");
+    fileOut = new File("result.asm");
 
     ArrayList<Parser> parsers = new ArrayList<>();
 
@@ -28,18 +31,20 @@ public class Main {
       Parser parser = new Parser(file);
       parsers.add(parser);
     }
-  }
 
-  public static ArrayList<File> getVMFiles(File directory) {
-    File[] files = directory.listFiles();
-    ArrayList<File> allFiles = new ArrayList<>();
-    if (files != null) {
-      for (File file : files) {
-        if (file.getName().endsWith(".vm")) allFiles.add(file);
+    CodeWriter codeWriter = new CodeWriter(fileOut);
+
+    for (Parser parser : parsers) {
+      while (parser.hasMoreCommands()) {
+        parser.advance();
+        if (parser.getCommandType().equals(C_ARITHMETIC)) {
+          codeWriter.writeArithmetic(parser.arg1());
+        }
+        else if (parser.getCommandType().equals(C_PUSH) || parser.getCommandType().equals(C_POP)) {
+          codeWriter.writePushPop(parser.getCommandType(), parser.arg1(), parser.arg2());
+        }
       }
     }
-    return allFiles;
-
   }
 
   public static ArrayList<File> getVMFiles(File directory) {
