f4263161d04ae1744cf670b4589ad80f107c46af
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-06-14 20:03:47 +0200

    get the jack code without comments or something else

--- Project_10/src/compiler/JackTokenizer.java
+++ Project_10/src/compiler/JackTokenizer.java
@@ -31,6 +31,88 @@ public class JackTokenizer {
       e.printStackTrace();
     }
 
+    jackcode = "";
+    while (mScanner.hasNextLine()) {
+      String strLine = mScanner.nextLine();
+      while (strLine.equals("") || hasComments(strLine)) {
+        if (hasComments(strLine)) {
+          strLine = removeComments(strLine);
+        }
+        if (strLine.trim().equals("")) {
+          if (mScanner.hasNextLine()) {
+            strLine = mScanner.nextLine();
+          } else {
+            break;
+          }
+        }
+      }
+      jackcode += strLine.trim();
+    }
+    // add tokens from the jackcode string into an arraylist of the tokens
+    tokens = new ArrayList<String>();
+    while (jackcode.length() > 0) {
+      while (jackcode.charAt(0) == ' ') {
+        jackcode = jackcode.substring(1);
+      }
+      // keyword
+      for (int i = 0; i < keyWords.size(); i++) {
+        if (jackcode.startsWith(keyWords.get(i).toString())) {
+          String keyword = keyWords.get(i).toString();
+          tokens.add(keyword);
+          jackcode = jackcode.substring(keyword.length());
+        }
+
+      }
+      // symbol
+      if (symbols.contains(jackcode.substring(0, 1))) {
+        char symbol = jackcode.charAt(0);
+        tokens.add(Character.toString(symbol));
+        jackcode = jackcode.substring(1);
+      }
+      // integer constant
+      else if (Character.isDigit(jackcode.charAt(0))) {
+        String value = jackcode.substring(0, 1);
+        jackcode = jackcode.substring(1);
+        while (Character.isDigit(jackcode.charAt(0))) {
+          value += jackcode.substring(0, 1);
+          jackcode = jackcode.substring(1);
+
+        }
+        tokens.add(value);
+
+      }
+      // string constant
+      else if (jackcode.substring(0, 1).equals("\"")) {
+        jackcode = jackcode.substring(1);
+        String strString = "\"";
+        while ((jackcode.charAt(0) != '\"')) {
+          strString += jackcode.charAt(0);
+          jackcode = jackcode.substring(1);
+
+        }
+        strString = strString + "\"";
+        tokens.add(strString);
+        jackcode = jackcode.substring(1);
+
+      }
+      // identifier
+      else if (Character.isLetter(jackcode.charAt(0)) || (jackcode.substring(0, 1).equals("_"))) {
+        String strIdentifier = jackcode.substring(0, 1);
+        jackcode = jackcode.substring(1);
+        while ((Character.isLetter(jackcode.charAt(0))) || (jackcode.substring(0, 1).equals("_"))) {
+          strIdentifier += jackcode.substring(0, 1);
+          jackcode = jackcode.substring(1);
+        }
+
+        tokens.add(strIdentifier);
+
+      }
+      // start out with pointer at position 0
+      bFirst = true;
+      pointer = 0;
+
+    }
+
   }
 
 }
