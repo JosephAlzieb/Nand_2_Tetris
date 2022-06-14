13e5a1ff1c684ad7d742812f0a422f3bfbfae471
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-06-14 22:01:29 +0200

    compiles a static declaration or a field declaration

--- Project_10/src/compiler/CompilationEngine.java
+++ Project_10/src/compiler/CompilationEngine.java
@@ -37,6 +37,54 @@ public class CompilationEngine {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  // compiles a static declaration or a field declaration
+  public void compileClassVarDec() {
+    jtoken.advance();
+    try {
+      while (jtoken.keyWord().equals("static") || jtoken.keyWord().equals("field")) {
+        fw.write("<classVarDec>\n");
+        // field or static
+        fw.write("<keyword> " + jtoken.keyWord() + " </keyword>\n");
+        jtoken.advance();
+        // if for example, field Square square (Square)
+        if (jtoken.tokenType().equals("IDENTIFIER")) {
+          fw.write("<identifier> " + jtoken.identifier() + "</identifier>\n");
+        }
+        // if for example, field int square (int)
+        else {
+          fw.write("<keyword> " + jtoken.keyWord() + "</keyword>\n");
+
+        }
+        jtoken.advance();
+        // third word of the classvardec - e.g. square in the above - field int square
+        fw.write("<identifier> " + jtoken.identifier() + "</identifier>\n");
+        jtoken.advance();
+        // if there are multiple in 1 line - e.g. field int x, y
+        if (jtoken.symbol() == ',') {
+          fw.write("<symbol> , </symbol>\n");
+          jtoken.advance();
+          fw.write(("<identifier> " + jtoken.identifier() + "</identifier>\n"));
+          jtoken.advance();
+        }
+        // semicolon
+        fw.write("<symbol> ; </symbol>\n");
+        jtoken.advance();
+        fw.write("</classVarDec>\n");
+      }
+
+      // if reach a subroutine, go back in the arraylist to accomodate for advance in the next call
+      if (jtoken.keyWord().equals("function") || jtoken.keyWord().equals("method") || jtoken.keyWord().equals("constructor")) {
+        jtoken.decrementPointer();
+        return;
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
 
   }
+
 }
